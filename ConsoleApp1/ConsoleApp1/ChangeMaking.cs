cd6cff112d426399c6580b3d6ff0e3a9b7a6cd4f
dziezak <01189082@pw.edu.pl>
2025-02-20 16:05:42 +0100

    etap 2

--- ConsoleApp1/ConsoleApp1/ChangeMaking.cs
+++ ConsoleApp1/ConsoleApp1/ChangeMaking.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.Win32.SafeHandles;
 using System;
+using System.Reflection.Metadata;
 
 namespace ASD
 {
@@ -91,52 +93,58 @@ namespace ASD
         /// Dodatkowa uzyta pamięć powinna (musi) być proporcjonalna do wartości iloczynu amount*(liczba rodzajów monet)
         /// ( czyli rzędu o(amount*(liczba rodzajów monet)) )
         /// </remarks>
+       
         public int? Dynamic(int amount, int[] coins, int[] limits, out int[] change)
         {
-
-            int[] T = new int[amount + 1];
-            int[] P = new int[amount + 1];
-
-            for (int i = 1; i <= amount; i++)
+            int n = coins.Length;
+            int[,] T = new int[n + 1, amount + 1];
+            int[,] P = new int[n + 1, amount + 1];
+            for(int i=1; i<=amount; i++)
             {
-                T[i] = int.MaxValue;
-                P[i] = -1;
+                T[0, i] = int.MaxValue;
+                P[0, i] = -1;
             }
-            T[0] = 0;
-            for (int kk = 1; kk <= amount; kk++)
+
+            for(int i=1; i<=n; i++)
             {
-                foreach (var coin in coins)
+                for(int j=1; j<=amount; j++)
                 {
-                    if (kk >= coin)
-                    {
-                        int c = T[kk - coin] + 1;
-                        if (c < T[kk] && c >= 0)
+                    T[i, j] = T[i - 1, j];
+                    P[i, j] = -1;
+                    if (coins[i-1] <= j) {
+                        for (int kk = 1; kk <= limits[i-1] && coins[i-1]*kk <= j; kk++)
                         {
-                            T[kk] = c;
-                            P[kk] = coin;
+                            if (T[i-1, j - kk * coins[i-1]] != int.MaxValue)
+                            {
+                                int newCount = T[i - 1, j - kk * coins[i - 1]] + kk;
+                                if(newCount < T[i, j])
+                                {
+                                    T[i, j] = newCount;
+                                    P[i, j] = kk;
+                                }
+                            }
                         }
                     }
                 }
             }
-
-            if (T[amount] == int.MaxValue)
+            if (T[n, amount] == int.MaxValue)
             {
                 change = null;
                 return null;
             }
 
-            change = new int[coins.Length];
-            int currentAmount = amount;
+            change = new int[n];
+            int remainingAmount = amount;
 
-            while (currentAmount > 0)
+            for(int i=n; i>0 && remainingAmount>0; i--)
             {
-                int coin = P[currentAmount];
-                int coinIndex = Array.IndexOf(coins, coin);
-                change[coinIndex]++;
-                currentAmount -= coin;
+                if (P[i, remainingAmount] != -1)
+                {
+                    change[i-1] = P[i, remainingAmount];
+                    remainingAmount -= change[i - 1] * coins[i - 1];
+                }
             }
-
-            return T[amount];
+            return T[n, amount];
         }
 
     }
