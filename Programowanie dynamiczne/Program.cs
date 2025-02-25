b313fd7fd32aaeaad3e2e96806660f0c48b898ef
dziezak <01189082@pw.edu.pl>
2025-02-25 23:19:42 +0100

    wystarczy

--- Programowanie dynamiczne/Program.cs
+++ Programowanie dynamiczne/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Runtime.Intrinsics.Arm;
+﻿//#define Reszta
+using System.Runtime.Intrinsics.Arm;
 using System.Threading.Channels;
+
 
 namespace Programowanie_dynamiczne
 {
@@ -7,10 +9,11 @@ namespace Programowanie_dynamiczne
     {
         static void Main(string[] args)
         {
-            int n = 10, k = 5;
+#if Reszta
+            int n = 15, k = 5;
             int[]? dp = new int[n+1];
             //int[] c = new int[k];
-            int[] c = { 1, 20, 10, 11, 25};
+            int[] c = { 2, 3, 12, 11, 25}; // moenty
 
 
             /*
@@ -36,10 +39,15 @@ namespace Programowanie_dynamiczne
                     dp[i] = Math.Min(dp[i - c[j]] + 1, dp[i]);
                 }
             }
+            for(int j  = 1; j<=n; j++)
+                Console.Write(j + " ");
+            Console.WriteLine();
             for(int j=1; j<=n;j++)
             {
                 Console.Write(dp[j]+" ");
             }
+#endif
+            
         }
 
     }
